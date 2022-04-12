290249eea68bd76fb27953d91f6b5d19e1c3df0e
Filipe Farias <filipe.farias.chagas@protonmail.com>
2022-04-12 02:02:15 -0300

    terminando api 2

--- src/test/java/tech/klok/challenge/DesafioEstagioApi2ApplicationTests.java
+++ src/test/java/tech/klok/challenge/DesafioEstagioApi2ApplicationTests.java
@@ -3,7 +3,7 @@ package tech.klok.challenge;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=DesafioEstagioApi2ApplicationTests.class)
 class DesafioEstagioApi2ApplicationTests {
 
 	@Test
